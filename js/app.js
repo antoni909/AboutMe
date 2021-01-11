15077e0b091bdb666ef367614f292cc0986c4c7c
antoni <lorenzo.antonio.090@gmail.com>
2021-01-11 10:23:55 -0800

    add js instructions via comments on app.js will allow for clear set of instructions and serve as a checklist when js code is being created

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
+// JS INSTRUCTIONS ARE AS FOLLOWS:
+
+// js file must contain 'use strict' declaration
+// js must be linked to html file as an externel script file
+// make sure all code is clean
+// correct indentation and syntax
+
+// JS CODE INSTRUCTIONS:
+// Prompt User 5x with Y/N questions
+// Consisder case sensitivity by normalizing user input (.toUpperCase or .toLowerCase)
+// Alert the user if they answered the question correctly or not
+// Add console.log() messages within app to notify if the are correct. Comment out these colsole.logs for submission
+// Replace console.logs with alerts() to complete the project
+
+
 'use strict';
