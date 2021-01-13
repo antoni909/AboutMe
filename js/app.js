52e8af9cbdd8d67e1e2ced449bb852c1034fd355
antoni <lorenzo.antonio.090@gmail.com>
2021-01-12 18:01:04 -0800

    add .toLowerCase to variables in five functions in the app.js to complete one more instruction from the assignment

--- js/app.js
+++ js/app.js
@@ -20,17 +20,18 @@ JS CODE INSTRUCTIONS:
 
 //Here I create an initial prompt question to capture the users name and use it in subsequent prompts
 
-var userName = prompt('Hello and Welcome to my page! What is your name?');
+var userNameResponse = prompt('Hello and Welcome to my page! What is your name?');
+var userName = userNameResponse.toLowerCase();
 console.log(userName);
 
-//Here I create the the subsequent prompt questions and positive confirmation to the user
+// //Here I create the the subsequent prompt questions and positive confirmation to the user
 
-// Question 1
+// // Question 1
 function questionOne() {
 
-  var initialResponse = prompt('Hello ' + userName + '!!! Welcome to my page, I will ask you five yes or no questions, is that okay?');
- 
-  if(initialResponse === 'yes') {
+  var initialResponse = prompt('Hello ' + userName + ', I will ask you five yes or no questions, is that okay?');
+  var userResponse = initialResponse.toLowerCase();
+  if(userResponse === 'yes') {
     return alert('That is amazing, ' + userName + ' ! Let\'s begin then :D');
   }else {
     return alert('It looks like you dont want to continue, ' + userName + ', thank you for visiting my page');
@@ -38,54 +39,57 @@ function questionOne() {
 }
 var responseOne = questionOne();
 
-// Question 2
-function questionTwo() {
+// // Question 2
+// function questionTwo() {
 
-  var userResp = prompt('Respond Yes or No to question 01: is the sky blue?');
+//   var userResp = prompt('Respond Yes or No to question 01: is the sky blue?');
+//   var userResponse = initialResponse.toLowerCase();
+//   if(userResponse === 'yes'){
+//     return alert('Great answer ' + userName +'!');
+//   }else {
+//     return alert( userName + ', you might want to rethink that, refresh the page and try again');
+//   }
+// }
+// var responseTwo = questionTwo();
 
-  if(userResp === 'yes'){
-    return alert('Great answer ' + userName +'!');
-  }else {
-    return alert( userName + ', you might want to rethink that, refresh the page and try again');
-  }
-}
-var responseTwo = questionTwo();
+// // Question 3
+// function questionThree() {
 
-// Question 3
-function questionThree() {
+//   var userResp = prompt( userName +', Are you currently enrolled in Code Fellows 201?');
+//   var userResponse = initialResponse.toLowerCase();
 
-  var userResp = prompt( userName +', Are you currently enrolled in Code Fellows 201?');
+//   if(userResponse === 'yes'){
+//     return alert('Me too! I am excited to get this course going and getting a good grasp on the fundumentals of JavaScript and HTML/CSS');
+//   }else {
+//     return alert('No? ' + userName + ', you might want to talk to admissions then, thank you for visiting');
+//   }
+// }
+// var responseThree = questionThree();
 
-  if(userResp === 'yes'){
-    return alert('Me too! I am excited to get this course going and getting a good grasp on the fundumentals of JavaScript and HTML/CSS');
-  }else {
-    return alert('No? ' + userName + ', you might want to talk to admissions then, thank you for visiting');
-  }
-}
-var responseThree = questionThree();
+// // Question 4
+// function questionFour() {
 
-// Question 4
-function questionFour() {
+//   var userResp = prompt('Will you be enrolling into Code Fellows 301 after this 201 coure?');
+//   var userResponse = initialResponse.toLowerCase();
 
-  var userResp = prompt('Will you be enrolling into Code Fellows 301 after this 201 coure?');
+//   if(userResponse === 'yes'){
+//     return alert('That\'s great to hear ' + userName + ', I am also planning on doing the same!');
+//   }else {
+//     return alert('I\'m sure you have a reason for your decision, ' + userName + ', best of luck to you.' );
+//   }
+// }
+// var responseFour = questionFour();
 
-  if(userResp === 'yes'){
-    return alert('That\'s great to hear ' + userName + ', I am also planning on doing the same!');
-  }else {
-    return alert('I\'m sure you have a reason for your decision, ' + userName + ', best of luck to you.' );
-  }
-}
-var responseFour = questionFour();
+// // Question 5
+// function questionFive() {
 
-// Question 5
-function questionFive() {
+//   var userResp = prompt('What about the 401 advanced course, are you planning on taking one of them in the near future?');
+//   var userResponse = initialResponse.toLowerCase();
 
-  var userResp = prompt('What about the 401 advanced course, are you planning on taking one of them in the near future?');
-
-  if(userResp === 'yes'){
-    return alert('Me too! I am thinking about taking Advanced Software Development in Full-Stack JavaScript');
-  }else {
-    return alert('Either way ' + userName + ', I am more than positive you have a bright future ahead of you' );
-  }
-}
-var responseFive = questionFive();
+//   if(userResponse === 'yes'){
+//     return alert('Me too! I am thinking about taking Advanced Software Development in Full-Stack JavaScript');
+//   }else {
+//     return alert('Either way ' + userName + ', I am more than positive you have a bright future ahead of you' );
+//   }
+// }
+// var responseFive = questionFive();
