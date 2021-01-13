79a876856c701d9404273533a00036936f387cde
antoni <lorenzo.antonio.090@gmail.com>
2021-01-12 16:05:21 -0800

    add js six functions and made first attempt at creating functional js code

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@ JS INSTRUCTIONS ARE AS FOLLOWS:
 - correct indentation and syntax
 
 JS CODE INSTRUCTIONS:
-- Prompt User 5x with Y/N questions
+-x Prompt User 5x with Y/N questions
 - Consisder case sensitivity by normalizing user input (.  toUpperCase or .toLowerCase)
 - Alert the user if they answered the question correctly or not
 - Add console.log() messages within app to notify if they are correct.
@@ -18,34 +18,74 @@ JS CODE INSTRUCTIONS:
 
 'use strict';
 
-var questionOne;
+//Here I create an initial prompt question to capture the users name and use it in subsequent prompts
 
-function userQuestionOne() {
-  var questionOne = prompt('Hello, Welcome to my Page! What is your name?');
+var userName = prompt('Hello and Welcome to my page! What is your name?');
+console.log(userName);
 
-  if (questionOne == questionOne) {
-    var compResponseOne = console.log('Your name is: ' + questionOne.toLowerCase + '!' + ' Welcome to my About Me page :D');
-    // var compResponse = alert('Your name is: ' + questionOne.toLowerCase + '!' + ' Welcome to my About Me page :D');
+//Here I create the the subsequent prompt questions and positive confirmation to the user
+
+// Question 1
+function questionOne() {
+
+  var initialResponse = prompt('Hello ' + userName + '!!! Welcome to my page, I will ask you five yes or no questions, is that okay?');
+ 
+  if(initialResponse === 'yes') {
+    return alert('That is amazing, ' + userName + ' ! Let\'s begin then :D');
   }else {
-    compResponseOne = console.log('That is unfortunate, refresh the page and try again');
-    // compResponse = alert('That is unfortunate, refresh the page and try again');
+    return alert('It looks like you dont want to continue, ' + userName + ', thank you for visiting my page');
   }
-  return compResponseOne;
 }
+var responseOne = questionOne();
 
-var userResponseOne = userQuestionOne();
+// Question 2
+function questionTwo() {
 
-function userQuestionTwo() {
-  var questionTwo = prompt('What color is the sky on clear and non-cloudy day?');
+  var userResp = prompt('Respond Yes or No to question 01: is the sky blue?');
 
-  if (questionTwo == ('sky blue' | 'blue' | 'light blue')) {
-    var compResponseTwo = console.log(questionOne + ' , your answer is correct! Great Job');
-    // var compResponse = alert(questionOne + ' , your answer is correct! Great Job');
+  if(userResp === 'yes'){
+    return alert('Great answer ' + userName +'!');
   }else {
-    compResponseTwo = console.log(questionOne + ' ,That is unfortunate, refresh the page and try again');
-    // compResponse = alert(questionOne + ' ,That is unfortunate, refresh the page and try again');
+    return alert( userName + ', you might want to rethink that, refresh the page and try again');
   }
-  return compResponseTwo;
 }
+var responseTwo = questionTwo();
 
-var userResponseTwo = userQuestionTwo(questionOne);
+// Question 3
+function questionThree() {
+
+  var userResp = prompt( userName +', Are you currently enrolled in Code Fellows 201?');
+
+  if(userResp === 'yes'){
+    return alert('Me too! I am excited to get this course going and getting a good grasp on the fundumentals of JavaScript and HTML/CSS');
+  }else {
+    return alert('No? ' + userName + ', you might want to talk to admissions then, thank you for visiting');
+  }
+}
+var responseThree = questionThree();
+
+// Question 4
+function questionFour() {
+
+  var userResp = prompt('Will you be enrolling into Code Fellows 301 after this 201 coure?');
+
+  if(userResp === 'yes'){
+    return alert('That\'s great to hear ' + userName + ', I am also planning on doing the same!');
+  }else {
+    return alert('I\'m sure you have a reason for your decision, ' + userName + ', best of luck to you.' );
+  }
+}
+var responseFour = questionFour();
+
+// Question 5
+function questionFive() {
+
+  var userResp = prompt('What about the 401 advanced course, are you planning on taking one of them in the near future?');
+
+  if(userResp === 'yes'){
+    return alert('Me too! I am thinking about taking Advanced Software Development in Full-Stack JavaScript');
+  }else {
+    return alert('Either way ' + userName + ', I am more than positive you have a bright future ahead of you' );
+  }
+}
+var responseFive = questionFive();
